737cdbc192714cf8b7d267832f1560f90d526044
`Aniket Borawake <131742395+Aniket026@users.noreply.github.com>
2024-02-22 21:02:33 +0530

    Add files via upload

---
+++ conditional_expressions.js
@@ -0,0 +1,21 @@
+let a = prompt("Enter your age")
+if(a>18)
+{
+    alert("You are aligibale to bick ride")
+}
+else
+{
+    alert("You are not  aligibale to bick ride")
+}
+
+// check if the number is positive
+
+const number = prompt("Enter a number: ");
+
+// check if number is greater than 0
+if (number > 0) {
+ // the body of the if statement
+  console.log("The number is positive");
+}
+
+console.log("The if statement is easy");
